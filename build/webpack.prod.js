c4784d99bc4824cb1db0072c0976afcda0b1d2fd
Yitian Zou <yitian.zou@shopee.com>
2022-08-17 10:33:01 +0800

    add image & media loader

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,7 +1,21 @@
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
-
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');  // 静态资源不需要webpack解析，直接在打包的时候把public下的内容复制到构建出口文件夹中
 module.exports = merge(baseConfig, {
   mode: 'production', // 生产模式，会开启tree-shaking和压缩代码，以及其他优化
-
+  plugins: [
+    // 复制文件插件
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'), // 复制public下文件
+          to: path.resolve(__dirname, '../dist'), // 复制到dist目录中
+          filter: source => {
+            return !source.includes('index.html') // 忽略index.html
+          }
+        }
+      ]
+    })
+  ]
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import './app.css'
+import './App.css';
+import './App.scss';
+import smallImg from './assets/imgs/5kb.png';
+import bigImg from './assets/imgs/22kb.png';
 
 function App() {
-  return <h2>module-federation-explore</h2>
+  return (
+    <>
+      <h2>react-ts-webpack5</h2>
+      <img src={smallImg} alt="小于10kb的图片" />
+      <img src={bigImg} alt="大于于10kb的图片" />
+    </>
+  );
 }
 
 export default App
