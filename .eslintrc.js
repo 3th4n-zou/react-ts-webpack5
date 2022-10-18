9727a83577d158cefe73a90b841bfb67e8032e01
3th4n <635985746@qq.com>
2022-10-18 11:37:12 +0800

    modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,78 +1,70 @@
 module.exports = {
-  "plugins": ["prettier", "@typescript-eslint"],
-  "extends": ["airbnb-typescript", "react-app", "prettier"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "./tsconfig.json"
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: ['airbnb-typescript', 'react-app', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
   },
-  "settings": {
-    "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true
-      }
-    }
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
-  "rules": {
-    "object-curly-spacing": ["warn", "always"],
-    "no-unused-vars": [
-      "warn",
+  rules: {
+    'object-curly-spacing': ['warn', 'always'],
+    'no-unused-vars': [
+      'warn',
       {
-        "vars": "all",
-        "args": "none"
-      }
+        vars: 'all',
+        args: 'none',
+      },
     ],
-    "@typescript-eslint/semi": [
-      "off"
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    '@typescript-eslint/semi': ['off'],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        "vars": "all",
-        "args": "none"
-      }
+        vars: 'all',
+        args: 'none',
+      },
     ],
-    "@typescript-eslint/no-explicit-any": [
-      "error",
+    '@typescript-eslint/no-explicit-any': [
+      'error',
       {
-        "ignoreRestArgs": true
-      }
+        ignoreRestArgs: true,
+      },
     ],
-    "max-len": [
-      "warn",
+    'max-len': [
+      'warn',
       {
-        "code": 120,
-        "ignoreStrings": true,
-        "ignoreTemplateLiterals": true,
-        "ignoreComments": true
-      }
+        code: 120,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+      },
     ],
-    "no-plusplus": [
-      "error",
+    'no-plusplus': [
+      'error',
       {
-        "allowForLoopAfterthoughts": true
-      }
+        allowForLoopAfterthoughts: true,
+      },
     ],
-    "react/jsx-key": "error",
-    "import/no-extraneous-dependencies": [
-      "error",
+    'react/jsx-key': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        "devDependencies": [
-          "**/*.test.js",
-          "**/*.test.jsx",
-          "**/*.test.ts",
-          "**/*.test.tsx",
-          "src/tests/**/*"
-        ]
-      }
+        devDependencies: ['**/*.test.js', '**/*.test.jsx', '**/*.test.ts', '**/*.test.tsx', 'src/tests/**/*'],
+      },
     ],
-    "react/jsx-props-no-spreading": "off",
-    "import/prefer-default-export": "off",
-    "react/jsx-boolean-value": "off",
-    "react/prop-types": "off",
-    "react/no-unescaped-entities": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/jsx-wrap-multilines": "off",
-    "react/destructuring-assignment": "off",
-    "react/self-closing-comp": ["error"]
-  }
-}
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-boolean-value': 'off',
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-wrap-multilines': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/self-closing-comp': ['error'],
+  },
+};
